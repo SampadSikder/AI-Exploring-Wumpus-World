bda917c7423aa1a5517e186e708ef0857e06e20d
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-09-19 12:59:53 +0600

    added unage ub gyu

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,136 @@
-// Create a grid of squares
-const grid = [[null, null, null],
-              [null, null, null],
-              [null, null, null]];
+const GOLD = {
+    "piece": "G",
+    "piece_name":"Gold",
+    "image": "./resources/wumpus/gold.gif"
+};
 
-    // Add the Wumpus and the treasure to the grid
-grid[1][1] = "Wumpus";
-grid[2][2] = "Gold";
+const PIT = {
+    "piece": "P",
+    "piece_name":"Pit",
+    "image": "./resources/wumpus/pitc.gif"
+};
 
-// Create a function to draw the grid
-function drawGrid() {
+const WUMPUS = {
+    "piece": "W",
+    "piece_name":"Wumpus",
+    "image": "./resources/wumpus/wumpusc.gif"
+};
+
+const BREEZE = {
+    "piece": "B",
+    "piece_name":"Breeze"
+};
+
+const STENCH = {
+    "piece": "S",
+    "piece_name":"Stench"
+};
+
+const EXPLORED_CELL = {
+    "piece": "C",
+    "piece_name":"Explored",
+    "image": "./resources/floor.png"
+}
+const UNEXPLORED_CELL = {
+    "piece": "C",
+    "piece_name":"Unexplored",
+    "image": "./resources/wumpus/original_wall.gif"
+}
+const NORMAL_CELL = {
+    "piece": "C",
+    "piece_name":"Normal",
+    "image": "./resources/wumpus/original_wall.gif"
+};
+
+const AGENT = {
+    "piece": "A",
+    "piece_name":"Agent"
+}
+
+let wumpusWorld = [];
+let exploredWorld = [];
+
+function generate_world(){
+    for (let i = 0; i < 10; i++) {
+        let row = [];
+        for (let j = 0; j < 10; j++) {
+            const randomValue = Math.random();
+
+            if (randomValue < 0.1) {
+                row.push(GOLD);
+            } else if (randomValue < 0.3) {
+                row.push(PIT);
+            } else if (randomValue < 0.4) {
+                row.push(WUMPUS);
+            } else {
+                row.push(NORMAL_CELL);
+            }
+        }
+        wumpusWorld.push(row);
+    }
+
+    exploredWorld = JSON.parse(JSON.stringify(wumpusWorld));
+    
+    for(let i=0; i<exploredWorld.length; i++)
+        for(let j=0; j<exploredWorld.length; j++)
+            exploredWorld[i][j] = UNEXPLORED_CELL;
+
+    wumpusWorld[0][0] = AGENT;
+    exploredWorld[0][0] = EXPLORED_CELL;
+
+    console.log(wumpusWorld);
+}
+
+
+function drawOriginalWorld() {
     const gridElement = document.getElementById("grid");
     
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            const square = grid[i][j];
+    for (let i = 0; i < wumpusWorld.length; i++) {
+        for (let j = 0; j < wumpusWorld[i].length; j++) {
+            const cellValue = wumpusWorld[i][j];
 	    
-            const squareElement = document.createElement("div");
+            const squareElement = document.createElement("img");
             squareElement.classList.add("square");
+            
+            squareElement.innerText = cellValue.piece_name;
+
+            if (cellValue["piece_name"] == "Wumpus") {
+		        squareElement.setAttribute("src", WUMPUS.image);
+            } else if (cellValue["piece_name"] === "Gold") {
+                squareElement.setAttribute("src", GOLD.image);
+            } else if (cellValue["piece_name"] === "Pit") {
+		        squareElement.setAttribute("src", PIT.image);
+            } else if(cellValue["piece_name"]=="Normal"){
+                squareElement.setAttribute("src", NORMAL_CELL.image);
+            }
 	    
-            if (square === "Wumpus") {
-		squareElement.classList.add("wumpus");
-            } else if (square === "Gold") {
-		squareElement.classList.add("gold");
+            gridElement.appendChild(squareElement);
+        }
+    }
+}
+
+function drawExploredWorld() {
+    const gridElement = document.getElementById("grid");
+    
+    for (let i = 0; i < wumpusWorld.length; i++) {
+        for (let j = 0; j < wumpusWorld[i].length; j++) {
+            const cellValue = wumpusWorld[i][j];
+	    
+            const squareElement = document.createElement("img");
+            squareElement.classList.add("square");
+            
+            squareElement.innerText = cellValue.piece_name;
+
+            if (cellValue["piece_name"] == "Wumpus") {
+		        squareElement.setAttribute("src", WUMPUS.image);
+            } else if (cellValue["piece_name"] === "Gold") {
+                squareElement.setAttribute("src", GOLD.image);
+            } else if (cellValue["piece_name"] === "Pit") {
+		        squareElement.setAttribute("src", PIT.image);
+            } else if(cellValue["piece_name"]=="Explored"){
+                squareElement.setAttribute("src", EXPLORED_CELL.image);
+            } else if(cellValue["piece_name"]=="Unexplored"){
+                squareElement.setAttribute("src", UNEXPLORED_CELL.image);
             }
 	    
             gridElement.appendChild(squareElement);
