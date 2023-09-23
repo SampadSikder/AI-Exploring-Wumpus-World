b78ea25353bd4744249e2c5d2056fcca24982384
Sampad Sikder <60497178+SampadSikder@users.noreply.github.com>
2023-09-23 21:35:26 +0600

    completed api connection

--- backend/WumpusBackend/src/api/ai.rs
+++ backend/WumpusBackend/src/api/ai.rs
@@ -6,21 +6,18 @@ use actix_web::{
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-struct World {
-    arrays: Vec<Vec<i32>>,
+struct Cell {
+    x: i32,
+    y: i32,
+    perceived_environment:String,
+    arrows: i32
 }
 
 #[post("/ai/explore")]
 pub async fn start_explore(
-    exploredWorld: web::Json<World>,
-    wumpusWorld: web::Json<World>,
+    cell: web::Json<Cell>
 ) -> HttpResponse {
-    let world_data = exploredWorld.into_inner();
-    
-    for row in &world_data.arrays {
-        for val in row {
-            println!("Value: {}", val);
-        }
-    }
-    HttpResponse::Ok().json(world_data)
+    let cell_data = cell.into_inner();
+    println!("perceived_environment: {}", cell_data.perceived_environment);
+    HttpResponse::Ok().json(cell_data)
 }
